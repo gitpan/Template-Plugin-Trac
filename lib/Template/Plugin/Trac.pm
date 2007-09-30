909610088e8dac225adc130dd3171b30e93e8450
Graham TerMarsch <cpan@howlingfrog.com>
2007-09-30 23:03:43 +0000

    Import of GTERMARS/Template-Plugin-Trac-1.01 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Trac
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         GTERMARS/Template-Plugin-Trac-1.01.tar.gz
    gitpan-cpan-author:       GTERMARS
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/Trac.pm
+++ lib/Template/Plugin/Trac.pm
@@ -1,12 +1,23 @@
 package Template::Plugin::Trac;
 
+###############################################################################
+# Required inclusions.
+###############################################################################
 use strict;
 use warnings;
 use Text::Trac;
 use base qw(Template::Plugin::Filter);
 
-our $VERSION = '1.000';
+###############################################################################
+# Version number.
+###############################################################################
+our $VERSION = '1.01';
 
+###############################################################################
+# Subroutine:   init()
+###############################################################################
+# Initializes the template plugin.
+###############################################################################
 sub init {
     my $self = shift;
     $self->{'_DYNAMIC'} = 1;
@@ -14,6 +25,11 @@ sub init {
     return $self;
 }
 
+###############################################################################
+# Subroutine:   filter($txt, $args, $config)
+###############################################################################
+# Filters the given text, by running it through 'Text::Trac'.
+###############################################################################
 sub filter {
     my ($self, $text, $args, $config) = @_;
     my $parser = Text::Trac->new( %{$config} );
@@ -24,7 +40,7 @@ sub filter {
 
 =head1 NAME
 
-Template::Plugin::TextTrac - TT filter for Text::Trac
+Template::Plugin::Trac - TT filter for Text::Trac
 
 =head1 SYNOPSIS
 
@@ -39,8 +55,22 @@ Template::Plugin::TextTrac - TT filter for Text::Trac
 
 =head1 DESCRIPTION
 
-C<Template::Plugin::TextTrac> is a plugin for TT, which formats your text using
+C<Template::Plugin::Trac> is a plugin for TT, which formats your text using
 Trac's WikiFormatting style.
+
+=head1 METHODS
+
+=over
+
+=item init()
+
+Initializes the template plugin. 
+
+=item filter($txt, $args, $config)
+
+Filters the given text, by running it through C<Text::Trac>. 
+
+=back
 
 =head1 AUTHOR
 
@@ -48,7 +78,7 @@ Graham TerMarsch <cpan@howlingfrog.com>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2006, Graham TerMarsch.  All rights reserved.
+Copyright (C) 2006-2007, Graham TerMarsch.  All rights Reserved.
 
 This is free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
